e4e1f86c9d045c3349f238e73ccacc7d5ee350fc
Thaís Bitencourt <thais.bitencourt@yahoo.com>
2022-10-30 15:27:21 -0300

    add integer validator and tests

--- __tests__/unit/validators.test.js
+++ __tests__/unit/validators.test.js
@@ -1,5 +1,5 @@
 const { describe, it, expect } = require("@jest/globals")
-const { isStringValid, isArrayValid, isObjectValid } = require("../../src/validators")
+const { isStringValid, isIntegerValid, isArrayValid, isObjectValid } = require("../../src/validators")
 
 describe('Validators test suite', () => {
     describe('isStringValid test suite', () => {
@@ -20,10 +20,30 @@ describe('Validators test suite', () => {
         })
     })
 
-    describe('isStringInteger test suite', () => {
-        it.todo('should return false if value is not a number')
-        it.todo('should return false if value is smaller than minimal')
-        it.todo('should return false if value is bigger than maximal')
+    describe('isIntegerValid test suite', () => {
+        it('should return true if integer meets the requirements', () => {
+            expect(isIntegerValid({ integer: 1, minimal: 1, maximal: 1 })).toBe(true)
+        })
+
+        it('should return true if value is integer and minal and maximal are not specified', () => {
+            expect(isIntegerValid({ integer: 1 })).toBe(true)
+        })
+
+        it('should work if parameters are stringified', () => {
+            expect(isIntegerValid({ integer: '100', minimal: '100', maximal: '105' })).toBe(true)
+        })
+
+        it('should return false if value cannot be converted to number', () => {
+            expect(isIntegerValid({ integer: undefined })).toBe(false)
+        })
+
+        it('should return false if value is smaller than minimal', () => {
+            expect(isIntegerValid({ integer: 1, minimal: 2 })).toBe(false)
+        })
+        
+        it('should return false if value is bigger than maximal', () => {
+            expect(isIntegerValid({ integer: 4, maximal: 3 })).toBe(false)
+        })
     })
 
     describe('isArrayValid test suite', () => {
--- src/validators/index.js
+++ src/validators/index.js
@@ -12,10 +12,24 @@ const isStringValid = ({ string, valuesEnum = [], pattern }) => {
   return true
 }
 
-const isIntegerValid = ({ integer, minimal, maximal }) => {}
+const isIntegerValid = ({ integer, minimal, maximal }) => {
+    const convertedInteger = Number(integer)
+    const convertedMinimal = Number(integer)
+    const convertedMaximal = Number(maximal)
+
+    const isValidNumber = number => !Number.isNaN(number)
+    
+    if (!isValidNumber(convertedInteger)) return false
+    
+    if (isValidNumber(convertedMinimal) && convertedInteger < minimal) return false
+    
+    if (isValidNumber(convertedMaximal) && convertedInteger > convertedMaximal) return false
+
+    return true
+}
 
 const isArrayValid = ({ array, minItems, maxItems, items }) => {}
 
 const isObjectValid = ({ additionalProperties, required, properties }) => {}
 
-module.exports = { isStringValid, isArrayValid, isObjectValid }
+module.exports = { isStringValid, isIntegerValid, isArrayValid, isObjectValid }
