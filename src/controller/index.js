2cb0a00a914578c4567a1f7788762ffecd8c9942
Thaís Bitencourt <thais.bitencourt@yahoo.com>
2022-10-30 07:15:43 -0300

    add controller logic

--- src/controller/index.js
+++ src/controller/index.js
@@ -1,9 +1,30 @@
+const { calculateAverage, calculateAnualCO2Economy } = require('../helpers')
 const { eligibilityService } = require('../service')
 
 const eligibilityController = (request, response) => {
-  const result = eligibilityService(request.body)
+  const {
+    tipoDeConexao,
+    classeDeConsumo,
+    modalidadeTarifaria,
+    historicoDeConsumo
+  } = request.body
 
-  response.json(result)
+  const averageConsumption = calculateAverage(historicoDeConsumo)
+
+  const { eligible, ineligibilityReasons } = eligibilityService({
+    averageConsumption,
+    connectionType: tipoDeConexao,
+    consumptionClass: classeDeConsumo,
+    billingModality: modalidadeTarifaria
+  })
+
+  const apiResponse = {
+    elegivel: eligible,
+    ...(!eligible && { razoesInelegibilidade: ineligibilityReasons }),
+    ...(eligible && { economiaAnualDeCO2: calculateAnualCO2Economy(averageConsumption) })
+  }
+
+  response.status(200).json(apiResponse)
 }
 
 module.exports = { eligibilityController }
