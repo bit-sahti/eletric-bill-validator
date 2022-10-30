2ade27f94882c33607371f058b8ff83074385d0b
Thaís Bitencourt <thais.bitencourt@yahoo.com>
2022-10-30 07:15:26 -0300

    add mockExpress and fix typos

--- __tests__/unit/controller.test.js
+++ __tests__/unit/controller.test.js
@@ -6,29 +6,33 @@ const {
   calculateAnualCO2Economy
 } = require('../../src/helpers')
 const { eligibilityService } = require('../../src/service')
-const { EletricBillBuilder } = require('./builder')
+const { mockExpress } = require('./utils')
 
 jest.mock('../../src/service')
 jest.mock('../../src/helpers')
 
 describe('Elegibility Controller test suite', () => {
-  const request = {
-    body: {
+  let request, response
+
+  beforeEach(() => {
+    const express = mockExpress()
+    
+    request = express.request
+    response = express.response
+
+    request.body = {
       numeroDoDocumento: '14041737706',
       tipoDeConexao: 'bifasico',
       classeDeConsumo: 'comercial',
       modalidadeTarifaria: 'convencional',
       historicoDeConsumo: [1000, 1000, 1000]
     }
-  }
-
-  const response = {
-    json: jest.fn()
-  }
+  })
 
   it('should get the average consumption and pass it to the service', () => {
     const mockedAverageConsumption = 1000
     calculateAverage.mockReturnValue(mockedAverageConsumption)
+    eligibilityService.mockReturnValue({})
 
     eligibilityController(request, response)
 
@@ -51,18 +55,20 @@ describe('Elegibility Controller test suite', () => {
 
   it('should get and return the CO2 economy projection if client is eligible', () => {
     const mockedAverageConsumption = 1000
-    const mockedCO2Economy = 2000
-    
+    const mockedCO2Economy = 84
+
     calculateAverage.mockReturnValue(mockedAverageConsumption)
     eligibilityService.mockReturnValue({
-      eligigle: true,
-      inegibilityReasons : []
+      eligible: true,
+      ineligibilityReasons: []
     })
     calculateAnualCO2Economy.mockReturnValue(mockedCO2Economy)
 
     eligibilityController(request, response)
 
-    expect(calculateAnualCO2Economy).toHaveBeenCalledWith(mockedAverageConsumption)
+    expect(calculateAnualCO2Economy).toHaveBeenCalledWith(
+      mockedAverageConsumption
+    )
     expect(response.json).toHaveBeenCalledWith({
       elegivel: true,
       economiaAnualDeCO2: mockedCO2Economy
@@ -70,11 +76,11 @@ describe('Elegibility Controller test suite', () => {
   })
 
   it('should get and return the ineligibility reasons if client is ineligible', () => {
-    const inegibilityReasons = eligibilityConstants.errors
+    const ineligibilityReasons = eligibilityConstants.errors
 
     eligibilityService.mockReturnValue({
-      eligigle: false,
-      inegibilityReasons
+      eligible: false,
+      ineligibilityReasons
     })
 
     eligibilityController(request, response)
@@ -82,7 +88,7 @@ describe('Elegibility Controller test suite', () => {
     expect(calculateAnualCO2Economy).not.toHaveBeenCalled()
     expect(response.json).toHaveBeenCalledWith({
       elegivel: false,
-      razoesInelegibilidade: inegibilityReasons
+      razoesInelegibilidade: ineligibilityReasons
     })
   })
 })
---
+++ __tests__/unit/utils/index.js
@@ -0,0 +1,17 @@
+const mockExpress = () => {
+    const request = {
+        body: {},
+        query: {},
+        params: {},
+        headers: {}
+    }
+
+    const response = {}
+
+    response.status = jest.fn().mockReturnValue(response)
+    response.json = jest.fn().mockReturnValue(response)
+
+    return { request, response }
+}
+
+module.exports = { mockExpress }
