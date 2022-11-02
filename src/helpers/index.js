81e863d23bb528c7301b1fd219787a0c4d68c51d
Thaís Bitencourt <thais.bitencourt@yahoo.com>
2022-11-02 19:02:48 -0300

    round the calculateAnualCO2Economy result

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -9,7 +9,10 @@ const calculateAnualCO2Economy = averageConsumption => {
   const carbonPer1000Kwh = 84
   const anualConsumptionQuotient = anualConsumption / 1000
 
-  return anualConsumptionQuotient * carbonPer1000Kwh
+  const anualCO2Economy = anualConsumptionQuotient * carbonPer1000Kwh
+
+  //toFixed appends zeros if the number has no decimal places, but the conversion back to number eliminates them
+  return Number(anualCO2Economy.toFixed(2))
 }
 
 const getRandomValuefromArray = array => {
