04004cb7aec0bdfe3cb411067572b0041d10cc52
Thaís Bitencourt <thais.bitencourt@yahoo.com>
2022-11-04 07:54:10 -0300

    connect to mongo and add model

---
+++ src/config/index.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose')
+
+const connectToMongo = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URL)
+
+        console.log(`Connected to database.`)
+    } catch (error) {
+        console.error('Failed to connect to database', error)
+    }
+}
+
+module.exports = { connectToMongo }
---
+++ src/model/index.js
@@ -0,0 +1,48 @@
+const mongoose = require('mongoose')
+const { eligibilityConstants } = require('../constants')
+const { mergeNestedArrays } = require('../helpers')
+
+const EligibilityCheckHistorySchema = new mongoose.Schema({
+    documentNumber: {
+        type: String,
+        required: true,
+        match: /^\d{11}$|^\d{14}$/
+    },
+    eligible: {
+        type: Boolean,
+        required: true
+    },
+    connectionType: {
+        type: String,
+        required: true,
+        enum: eligibilityConstants.connectionTypes
+    },
+    consumptionClass: {
+        type: String,
+        required: true,
+        enum: mergeNestedArrays(eligibilityConstants.consumptionClasses)
+    },
+    billingModality: {
+        type: String,
+        required: true,
+        enum: mergeNestedArrays(eligibilityConstants.billingModalities)
+    },
+    averageConsumption: {
+        type: Number,
+        required: true
+    },
+    anualCO2Economy: {
+        type: Number,
+        required: false
+    },
+    consumptionHistory: {
+        type: [{
+            type: Number,
+            required: true,
+            min: 0,
+            max: 9999
+        }]
+    }
+})
+
+module.exports = new mongoose.model('EligibilityCheckHistory', EligibilityCheckHistorySchema)
